57e2ae33faf3f4b833fe9a4d69f7e4e6f1d51844
daiemna <daiem.dna@gmail.com>
2015-02-28 21:40:30 +0100

    CascadedClassifier Base Added

---
+++ include/CascadedClassifier.hpp
@@ -0,0 +1,34 @@
+//CascadedClassifier.hpp
+
+#ifndef CASCADED_CLASSIFIER_HPP
+#define CASCADED_CLASSIFIER_HPP
+
+#include <opencv.hpp>
+
+using namespace cv;
+
+namespace ml{
+
+	class BoostedCascade : public CvStatModel {
+	public:
+		BoostedCascade(CvStatModel classifier);
+	    ~BoostedCascade();
+	    void clear();
+	    void save( const char* filename, const char* name=0 );
+	    void load( const char* filename, const char* name=0 );
+	    void write( CvFileStorage* storage, const char* name );
+	    void read( CvFileStorage* storage, CvFileNode* node );
+	    bool train(const Mat& train_data, const Mat& responses, int tflag = CV_COL_SAMPLE, const Mat& var_idx =0, const Mat& sample_idx = 0);
+	    float predict(const Mat& sample) const;
+	private:
+		unsigned int cascadeCount;
+		CvStatModel cascadeClassifier;
+		Mat featureIndices;
+		Mat sampleIndices;
+
+	};
+
+}
+
+
+#endif // CASCADED_CLASSIFIER_HPP
