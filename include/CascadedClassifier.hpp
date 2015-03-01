8c89d4302146b128415fa9ecd2c5fe9410013cb8
daiemna <daiem.dna@gmail.com>
2015-03-02 00:59:35 +0100

    added cascade list

--- include/CascadedClassifier.hpp
+++ include/CascadedClassifier.hpp
@@ -7,28 +7,28 @@
 
 using namespace cv;
 
-namespace ml{
+namespace ml {
 
-	class BoostedCascade : public CvStatModel {
-	public:
-		BoostedCascade(CvStatModel classifier);
-	    ~BoostedCascade();
-	    void clear();
-	    void save( const char* filename, const char* name=0 );
-	    void load( const char* filename, const char* name=0 );
-	    void write( CvFileStorage* storage, const char* name );
-	    void read( CvFileStorage* storage, CvFileNode* node );
-	    bool train(const Mat& train_data, const Mat& responses, int tflag = CV_COL_SAMPLE, const Mat& var_idx =0, const Mat& sample_idx = 0);
-	    float predict(const Mat& sample) const;
-	private:
-		unsigned int cascadeCount;
-		CvStatModel cascadeClassifier;
-		Mat featureIndices;
-		Mat sampleIndices;
+class CascadedClassifier: public CvStatModel {
+public:
+	CascadedClassifier(unsigned int cascadeCount, CvStatModel classifier);
+	~CascadedClassifier();
+	void clear();
+	void save(const char* filename, const char* name = 0);
+	void load(const char* filename, const char* name = 0);
+	void write(CvFileStorage* storage, const char* name);
+	void read(CvFileStorage* storage, CvFileNode* node);
+	bool train(const Mat& train_data, const Mat& responses, int tflag =
+			CV_COL_SAMPLE, const Mat& var_idx = 0, const Mat& sample_idx = 0);
+	float predict(const Mat& sample) const;
+private:
+//		unsigned int cascadeCount;
+	vector<CvStatModel> cascade_list;
+	Mat featureIndices;
+	Mat sampleIndices;
 
-	};
+};
 
 }
-
 
 #endif // CASCADED_CLASSIFIER_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,29 @@
 using namespace std;
 using namespace cv;
 
-int captureStream(int argc,char* argv[]);
+int captureStream(int argc, char* argv[]);
 
-int main(int argc,char* argv[]){
+int main(int argc, char* argv[]) {
 
-	return captureStream(argc,argv);
+	return captureStream(argc, argv);
 }
 
-int captureStream(int argc,char* argv[]){
+int captureStream(int argc, char* argv[]) {
 	VideoCapture cap(0); // open the default camera
-    if(!cap.isOpened())  // check if we succeeded
-        return -1;
+	if (!cap.isOpened())  // check if we succeeded
+		return -1;
 
-    Mat edges;
-    namedWindow("edges",1);
-    for(;;)
-    {
-        Mat frame;
-        cap >> frame; // get a new frame from camera
-        // cvtColor(frame, edges, CV_BGR2GRAY);
-        GaussianBlur(frame, edges, Size(7,7), 1.5, 1.5);
-        // Canny(edges, edges, 0, 30, 3);
-        imshow("edges", edges);
-        if(waitKey(30) >= 0) break;
-    }
-    return 0;
+	Mat edges;
+	namedWindow("edges", 1);
+	for (;;) {
+		Mat frame;
+		cap >> frame; // get a new frame from camera
+		// cvtColor(frame, edges, CV_BGR2GRAY);
+		GaussianBlur(frame, edges, Size(7, 7), 1.5, 1.5);
+		// Canny(edges, edges, 0, 30, 3);
+		imshow("edges", edges);
+		if (waitKey(30) >= 0)
+			break;
+	}
+	return 0;
 }
