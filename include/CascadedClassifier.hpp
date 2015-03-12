916e8d6c53a4c2a086036b4186867e84f096ba8e
daiemna <daiem.dna@gmail.com>
2015-03-12 23:40:25 +0100

    simple classifer is a preceptron

--- include/CascadedClassifier.hpp
+++ include/CascadedClassifier.hpp
@@ -9,21 +9,28 @@ using namespace cv;
 
 namespace ml {
 
+#define simple_classifier CvANN_MLP
+
+/******************************************************************************\
+*                          Cascaded Classifier                                *
+ \******************************************************************************/
+
+// Cascaded Classifier
 class CascadedClassifier: public CvStatModel {
 public:
-	CascadedClassifier(unsigned int cascadeCount, CvStatModel classifier);
+	CascadedClassifier(unsigned int cascadeCount);
 	~CascadedClassifier();
 	void clear();
 	void save(const char* filename, const char* name = 0);
 	void load(const char* filename, const char* name = 0);
 	void write(CvFileStorage* storage, const char* name);
 	void read(CvFileStorage* storage, CvFileNode* node);
-	bool train(const Mat& train_data, const Mat& responses, int tflag =
-			CV_COL_SAMPLE, const Mat& var_idx = 0, const Mat& sample_idx = 0);
+	bool train(const Mat& train_data, const Mat& responses, const Mat& var_idx =
+			0, const Mat& sample_idx = 0);
 	float predict(const Mat& sample) const;
 private:
 //		unsigned int cascadeCount;
-	vector<CvStatModel> cascade_list;
+	vector<simple_classifier> cascade_list;
 	Mat featureIndices;
 	Mat sampleIndices;
 
