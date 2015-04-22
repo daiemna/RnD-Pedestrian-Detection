5b307b71772ac89693cfad2288ba1b3123163f7e
daiemna <daiem.dna@gmail.com>
2015-04-22 11:31:15 +0200

    debuging flags and functions

---
+++ include/debug.hpp
@@ -0,0 +1,19 @@
+/*
+ *Debug.hpp
+ *Made by Daiem
+*/
+
+#ifndef DEBUG_HPP
+#define DEBUG_HPP
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+#define DEBUG_STREAM cout
+#define DEBUG_LOG printf
+#define ERROR_LOG printf
+
+#endif // DEBUG_HPP
+ 
